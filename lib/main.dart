2937fedfa9c8245645b7cfd577428b6896855f7f
Arslan Jameel <arslanch585@gmail.com>
2021-08-02 11:24:12 +0500

    Remove Debug Banner.

--- lib/main.dart
+++ lib/main.dart
@@ -64,6 +64,7 @@ class _MyAppState extends State<MyApp> {
     ];
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("Quiz App"),
